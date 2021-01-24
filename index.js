b8f37c91e50a755a921c632683edecc3b36ad6ad
Chuvetharani <pmuchuvetha@gmail.com>
2021-01-24 23:33:38 +0530

    backend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
